4d022fff1d21cb7c7e046ade8dd489da0915c78a
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2022-05-20 09:42:28 +0530

    Scroll Animations

--- src/world/world.js
+++ src/world/world.js
@@ -1,72 +1,93 @@
 import * as THREE from 'three';
-import {camera, scene, renderer, scrinit, resize} from './utilities/scr';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import Model from './../models/HCR_Race_Car.glb'
+import { Scene, PerspectiveCamera, WebGLRenderer, Color } from "three";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
-let geometry, material, mesh, animationScripts = [], scrollPercent = 0;
+let camera, scene, renderer;
+let mesh, animationScripts = [], scrollPercent = 0, loaded=false;
 
-init();
+camera = new PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.001, 1000);
+camera.position.set(0, 1, 2)
+scene = new Scene();
+scene.background = new Color('black')
+renderer = new WebGLRenderer({ antialias: true, alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls( camera, renderer.domElement );
+
+
+const loader = new GLTFLoader();
+
+loader.load(
+	Model,
+
+	function ( gltf ) {
+		scene.add( gltf.scene );
+		mesh = gltf.scene;
+		console.log(mesh);
+		loaded = true
+	},
+
+	function ( xhr ) {
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+	},
+
+	function ( error ) {
+		console.log( 'An error happened' );
+	}
+);
+
+
 window.scrollTo({ top: 0, behavior: 'smooth' })
-animate();
-
-document.body.onscroll = () => {
-    scrollPercent =
-        ((document.documentElement.scrollTop || document.body.scrollTop) /
-            ((document.documentElement.scrollHeight ||
-                document.body.scrollHeight) -
-                document.documentElement.clientHeight)) *
-    100;
-}
-
 
 animationScripts.push({
 	start: 0,
-    end: 101,
-    func: () => {
-        let g = material.color.g
-        g -= 0.05
-        if (g <= 0) {
-            g = 1.0
-        }
-        material.color.g = g
-    },
+	end: 40,
+	func: () => {
+		camera.lookAt(mesh.position)
+		mesh.position.z = lerp(-10, -5, scalePercent(0, 40))
+	},
 })
 
 animationScripts.push({
-    start: 0,
-    end: 40,
-    func: () => {
-        camera.lookAt(mesh.position)
-        camera.position.set(0, 1, 2)
-        mesh.position.z = lerp(-10, 0, scalePercent(0, 40))
-    },
+	start: 40,
+	end: 101,
+	func: () => {
+		camera.lookAt(mesh.position)
+		mesh.rotation.y = lerp(0, Math.PI, scalePercent(40, 60))
+	},
 })
 
-function init() {
-	scrinit("controls");
-	
-	geometry = new THREE.PlaneGeometry( 3, 3);
-	material = new THREE.MeshBasicMaterial({
-		color: 0x00ff00,
-		wireframe: true,
-	})
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
-}
-
 function animate() {
-
-	requestAnimationFrame( animate );
-	playScrollAnimations()
-	renderer.render( scene, camera );
-
+	requestAnimationFrame(animate);
+	if(loaded) playScrollAnimations()
+	renderer.render(scene, camera);
 }
+
+animate();
 
 //Event Listeners
-window.addEventListener('resize',resize);
+window.addEventListener('resize', resize);
 
+document.addEventListener('scroll',()=>{
+	scrollPercent =
+		((document.documentElement.scrollTop || document.body.scrollTop) /
+			((document.documentElement.scrollHeight ||
+				document.body.scrollHeight) -
+				document.documentElement.clientHeight)) *
+		100;
+})
+
+function resize(){
+	camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
 //Utility Functions
-function lerp(x, y, a){ 
-	return (1 - a) * x + a * y 
+function lerp(x, y, a) {
+	return (1 - a) * x + a * y
 }
 
 function scalePercent(start, end) {
@@ -74,9 +95,12 @@ function scalePercent(start, end) {
 }
 
 function playScrollAnimations() {
-    animationScripts.forEach((a) => {
-        if (scrollPercent >= a.start && scrollPercent < a.end) {
-            a.func()
-        }
-    })
+	animationScripts.forEach((a) => {
+		if (scrollPercent >= a.start && scrollPercent < a.end) {
+			a.func()
+		}
+	})
 }
+
+const light = new THREE.AmbientLight( 0xFFFFFF ); // soft white light
+scene.add( light );
