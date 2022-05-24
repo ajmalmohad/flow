91b802291f0adff6fb1bb53b3186d38682da4192
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2022-05-24 13:07:27 +0530

    Music

--- src/main.js
+++ src/main.js
@@ -1,2 +1,26 @@
 import './styles/main.scss';
 import './world/world';
+import Music from './music/Music.mp3'
+
+let playing = false;
+var audio = new Audio(Music);
+
+function play() {
+    if(!playing){
+        audio.play();
+        playing = true;
+    }else{
+        audio.currentTime = 0;
+        audio.pause();
+        playing = false;
+    }
+}
+
+document.getElementById("music").addEventListener('click',()=>{
+    if(!playing){
+        document.getElementById("music").innerHTML = "<button>Music On</button>";
+    }else{
+        document.getElementById("music").innerHTML = "<button>Music Off</button>";
+    }
+    play();
+})
