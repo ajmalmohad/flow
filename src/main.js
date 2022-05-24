9d9d4641eb5bce3208e7836ccf6a1f779186e512
Muhammed Ajmal <ajmalajmal.2016@gmail.com>
2022-05-24 17:09:10 +0530

    Loop Music

--- src/main.js
+++ src/main.js
@@ -5,21 +5,27 @@ let playing = false;
 var audio = new Audio(Music);
 
 function play() {
-    if(!playing){
+    if (!playing) {
         audio.play();
         playing = true;
-    }else{
+    } else {
         audio.currentTime = 0;
         audio.pause();
         playing = false;
     }
 }
 
-document.getElementById("music").addEventListener('click',()=>{
-    if(!playing){
+document.getElementById("music").addEventListener('click', () => {
+    if (!playing) {
         document.getElementById("music").innerHTML = "<button>Music On</button>";
-    }else{
+    } else {
         document.getElementById("music").innerHTML = "<button>Music Off</button>";
     }
     play();
 })
+
+audio.addEventListener('ended', function () {
+    audio.currentTime = 0;
+    audio.play();
+    playing = true;
+}, false);
